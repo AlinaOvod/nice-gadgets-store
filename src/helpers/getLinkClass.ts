10e1249cb0e3e0088bc10c20a57b0b5dbaec0a4f
Alina Ovod <alina.ipatova98@gmail.com>
2024-07-19 18:10:23 +0300

    some corrections

--- src/helpers/getLinkClass.ts
+++ src/helpers/getLinkClass.ts
@@ -4,7 +4,3 @@ export const getLinkClass = (isActive: boolean, className: string) =>
   classNames(`${className}`, {
     active: isActive,
   });
-// export const getLinkClass = (isActive: boolean, className: string) =>
-//   classNames(`${className}`, {
-//     active: isActive,
-//   });
